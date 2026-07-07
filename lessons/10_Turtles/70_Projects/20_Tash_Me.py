44af042b0f5781c1f63b0222bd9573d0f848c708
League Student <student@jointheleague.org>
2026-07-07 12:37:04 -0700

    Automated commit

--- lessons/10_Turtles/70_Projects/20_Tash_Me.py
+++ lessons/10_Turtles/70_Projects/20_Tash_Me.py
@@ -50,7 +50,7 @@ screen.setup(width=600, height=600)
 # Create a turtle and set its shape to the custom GIF
 t = turtle.Turtle()
 
-set_turtle_image(t, "moustache2.gif")
+set_turtle_image(t, "moustache1.gif")
 
 t.penup()   # Prevent drawing when moving
 t.speed(3)  # Set a moderate speed
