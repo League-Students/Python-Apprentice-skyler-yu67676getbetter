3f5ab9bfcfa433a91b618da3d36052bd1a4638bd
League Student <student@jointheleague.org>
2026-07-07 12:38:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/70_Projects/20_Tash_Me.py
+++ lessons/10_Turtles/70_Projects/20_Tash_Me.py
@@ -53,10 +53,6 @@ t = turtle.Turtle()
 set_turtle_image(t, "moustache1.gif")
 
 t.penup()   # Prevent drawing when moving
-t.speed(3)  # Set a moderate speed
 
-# Move the turtle to each corner of the screen in a square pattern
-for x, y in [(200, 200), (200, -200), (-200, -200), (-200, 200)]:
-    t.goto(x, y)
 
 turtle.exitonclick() 
